dbc5f01d2233951b0838289672adfa17360af145
Jeremy Coad <jeremymatthewcoad@gmail.com>
2026-06-04 22:45:38 +0000

    deploy update

--- data/nav.ts
+++ data/nav.ts
@@ -1,5 +1,6 @@
 export const navLinks = [
   { href: '#about', label: 'About' },
+  { href: '#hobbies', label: 'Hobbies' },
   { href: '#experiences', label: 'Experiences' },
   { href: '#featured-projects', label: 'Featured Projects' },
 ] as const;
